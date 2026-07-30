7d233f0d02353ee20bd7de09d4f4bd7d6929881e
punith-ctrl <letsmailpuni@gmail.com>
2026-07-30 08:24:53 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,21 +1,48 @@
 import mysql.connector
 from mysql.connector import Error
-from config import *
 
-def get_connection():
-    try:
-        connection = mysql.connector.connect(
-    host=DB_HOST,
-    user=DB_USER,
-    password=DB_PASSWORD,
-    database=DB_NAME
+from config import (
+    DB_HOST,
+    DB_USER,
+    DB_PASSWORD,
+    DB_NAME,
+    DB_PORT
 )
 
+
+def get_connection():
+
+    try:
+
+        connection = mysql.connector.connect(
+
+            host=DB_HOST,
+
+            port=DB_PORT,
+
+            user=DB_USER,
+
+            password=DB_PASSWORD,
+
+            database=DB_NAME,
+
+            connection_timeout=10
+
+        )
+
         if connection.is_connected():
-            print("✅ Database Connected Successfully")
+
+            print(
+                "Database Connected Successfully"
+            )
 
         return connection
 
-    except Error as e:
-        print(f"❌ Database Connection Error: {e}")
+    except Error as error:
+
+        print(
+            "Database Connection Error:",
+            error
+        )
+
         return None
